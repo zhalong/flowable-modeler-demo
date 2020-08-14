0da39f99459126232e4e0907bbd23ff654cf8ed0
“zhajianglong <zhajianglong@cpzs.org>
2020-08-14 19:08:34 +0800

    修改maven 及 tomcat版本

--- src/test/java/com/example/oldguy/services/ProcessDefinitionServiceTests.java
+++ src/test/java/com/example/oldguy/services/ProcessDefinitionServiceTests.java
@@ -1,7 +1,7 @@
 package com.example.oldguy.services;
 
 import com.example.oldguy.modules.app.services.ProcessDefinitionService;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
